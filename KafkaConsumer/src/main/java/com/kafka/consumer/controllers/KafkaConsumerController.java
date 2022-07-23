54e40d3af4a01df48ae35f840def8c59a48f5395
karoldd9 <karoldd9@gmail.com>
2022-07-23 21:39:22 +0200

    Done

--- KafkaConsumer/src/main/java/com/kafka/consumer/controllers/KafkaConsumerController.java
+++ KafkaConsumer/src/main/java/com/kafka/consumer/controllers/KafkaConsumerController.java
@@ -1,57 +1,25 @@
 package com.kafka.consumer.controllers;
 
-import com.kafka.consumer.runnable.ConsumerRunnable;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.*;
 
 @RestController
 @RequestMapping("/kafka")
 public class KafkaConsumerController {
 
-    private static Map<String, List<String>> messagesMap = new HashMap<>();
-    private Set<ConsumerRunnable> running = new HashSet<>();
-    Thread thread;
 
-    public static void addMessage(String topic, String message) {
-        System.out.println("Called with "+topic+" "+message);
-        List<String> tempMessages = new LinkedList<>();
 
-        if(messagesMap.containsKey(topic)) {
-            tempMessages = messagesMap.get(topic);
-        }
-        tempMessages.add(message);
-
-        messagesMap.put(topic, tempMessages);
-        System.out.println(messagesMap);
-        /*
-        if(messagesMap.containsKey(topic)) {
-            messagesMap.get(topic).add(message);
-        } else {
-            messagesMap.put(topic, List.of(message));
-        }
-         */
-    }
-
-    private void run(String topic, String consumerGroup) {
-        boolean alreadyExists = false;
-        for(ConsumerRunnable runnable: running) {
-            if(runnable.checkTopicAndConsumerGroup(topic, consumerGroup)) {
-                alreadyExists = true;
-            }
-        }
-
-        if(!alreadyExists) {
-            ConsumerRunnable consumerRunnable = new ConsumerRunnable(topic, consumerGroup);
-            thread = new Thread(consumerRunnable);
-            thread.start();
-
-            running.add(consumerRunnable);
-        }
-    }
 
     @GetMapping("/hej")
     public String hej() {
@@ -61,12 +29,34 @@ public class KafkaConsumerController {
     @GetMapping("/{topic}/{consumerGroup}")
     public List<String> messages(@PathVariable("topic") String topic, @PathVariable("consumerGroup") String consumerGroup) {
 
-        run(topic, consumerGroup);
 
-        List<String> topicMessages = messagesMap.containsKey(topic) ? messagesMap.get(topic) : List.of();
-        System.out.println("List: "+topicMessages);
-        System.out.println("Map1: "+messagesMap);
+        return pollData(topic, consumerGroup);
+    }
 
-        return topicMessages;
+
+    private KafkaConsumer<Integer, String> kafkaConsumer;
+
+    private List<String> pollData(String topic, String consumerGroup) {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        if(kafkaConsumer == null) {
+            kafkaConsumer = new KafkaConsumer<>(properties);
+        }
+
+        kafkaConsumer.subscribe(Arrays.asList(topic));
+
+        List<String> result = new LinkedList<>();
+
+        ConsumerRecords<Integer, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+        for(ConsumerRecord record: records) {
+            result.add(""+record.value());
+            System.out.println("Adding: "+record.value());
+        }
+        return result;
     }
 }
